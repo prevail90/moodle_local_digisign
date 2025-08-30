d3bd70e6ac7db4482c9eecf53444946a78c4217a
prevail90 <90661674+prevail90@users.noreply.github.com>
2025-08-31 01:48:32 +0900

    update version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_digisign';
-$plugin->version   = 2025083000; // YYYYMMDDXX
+$plugin->version   = 2025083001; // YYYYMMDDXX
 $plugin->requires  = 2022041900; // adjust to your Moodle minimum
 $plugin->maturity  = 'MATURITY_ALPHA';
-$plugin->release   = '0.1 (prototype)';
+$plugin->release   = '0.2 (prototype)';
